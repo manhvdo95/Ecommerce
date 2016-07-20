5e86629a21b40efeda442e99ade06787bc4c42a6
Manh Do <manhvdo95@yahoo.com>
2016-07-20 04:55:05 -0500

    more bootstrap - buttons

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ActiveRecord::Base
-	belongs_to :user
+  belongs_to :user
 
-	has_attached_file :image, styles: { medium: "400x600!" }
+  has_attached_file :image, styles: { medium: "400x600!", thumb: "200x300!" }
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
